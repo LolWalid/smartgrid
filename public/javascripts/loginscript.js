523aa6d361c6a12a1a1a4778e67171a90a3f8e79
Julien Kilo <julienkilo@gmail.com>
2015-01-07 23:09:26 +0100

    Ajout de variables de sessions (modif de package.json), gestion des connexions

---
+++ public/javascripts/loginscript.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	var login,password;
+	$("#login-form").submit(function(event){
+		pseudo = $("#pseudo").val();
+		password = $("#password").val();
+
+		if (pseudo == 0 && password === 'smartgrid') {
+			event.preventDefault();
+			window.location.href='/objectives';
+		}
+		else if (pseudo != '0') {
+			event.preventDefault();
+			var login = { login : pseudo};
+			console.log(login);
+			$.ajax({
+				url: '/login',
+				type: 'POST',
+				data: login,
+				dataType: 'json',
+			}).done(function( response ) {
+				if (response.msg === 'done')
+					window.location.href='/';
+			});
+		}
+		else {
+			alert('Mauvais mot de passe.');
+			$("#password").val('');
+			event.preventDefault();
+		}
+	});
+});
