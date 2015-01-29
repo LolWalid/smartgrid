e38ef77b7723c56bbdd6524c83625c42b9e573b3
WalidSeoudi <walid.seoudi@gmail.com>
2015-01-29 18:26:55 +0100

    debug socket.js

--- app/sockets.js
+++ app/sockets.js
@@ -1,5 +1,85 @@
-
 function addLog(message, db, type) {
+  switch(type) {
+    case 'transaction':
+      addTransaction(message, db)
+      break;
+    case 'vote':
+      addVote(message, db)
+      default:
+    break;
+  }
+}
+
+function addTransaction(message, db) {
+  log = {
+    type: 'transaction',
+    sender: message.joueur,
+    objectId: message.object,
+    action: message.action,
+  }
+
+  switch (message.action) {
+    case 'buy':
+      log.description = 'Player ' + message.joueur + ' bought an object.'
+      break
+    case 'sale':
+      log.description = 'Player ' + message.joueur + ' sold an object.'
+      break
+    case 'gift':
+      log.receiver = message.otherPlayer
+      log.description = 'Player ' + message.joueur + ' gave an object' + ' to player ' + message.otherPlayer + '.'
+      break
+    default:
+      break;
+  }
+  db.collection('logs').insert(log, function(err, result) {
+  });
+}
+
+function addVote(message, db) {
+}
+
+
+function sockets(io, db) {
+  io.sockets.on('connection', function(socket) {
+    socket.on('delete player', function(message) {
+      socket.broadcast.emit('server logout message', message);
+    });
+
+    socket.on('new objective', function(message) {
+      socket.broadcast.emit('server objective message', message);
+    });
+
+    socket.on('new event', function(message) {
+      socket.broadcast.emit('server event message', message);
+    });
+
+    socket.on('update view', function(message) {
+      socket.broadcast.emit('update view', message);
+    });
+
+    socket.on('new decision', function(message) {
+      socket.broadcast.emit('server decision message', message);
+    });
+
+    socket.on('response vote', function(message) {
+      addLog(message, db, 'vote')
+      socket.broadcast.emit('server decision response', message);
+    });
+
+    socket.on('action on object', function(message) {
+      addLog(message, db, 'transaction')
+      socket.broadcast.emit('server action on object', message);
+    });
+  });
+}
+
+module.exports.sockets = sockets;
+
+
+
+/*function addLog(message, db, type) {
+  console.log(message)
   var log =
       {
         sender : 'Admin',
@@ -38,46 +118,4 @@ function addLog(message, db, type) {
   }
   db.collection('logs').insert(log, function(err, result) {});
 }
-
-function sockets(io, db) {
-  io.sockets.on('connection', function(socket) {
-    //socket.broadcast.emit('new player', {});
-
-    socket.on('delete player', function(message) {
-      socket.broadcast.emit('server logout message', message);
-      addLog(message, db, 'logout')
-    });
-
-    socket.on('new objective', function(message) {
-      socket.broadcast.emit('server objective message', message);
-      addLog(message, db, 'new objective')
-    });
-
-    socket.on('new event', function(message) {
-     socket.broadcast.emit('server event message', message);
-      addLog(message, db, 'new event')
-    });
-
-    socket.on('update view', function(message) {
-      socket.broadcast.emit('update view', message);
-      addLog(message, db, 'update view')
-    });
-
-    socket.on('new decision', function(message) {
-      socket.broadcast.emit('server decision message', message);
-      addLog(message, db, 'new decision')
-    });
-
-    socket.on('response vote', function(message) {
-      socket.broadcast.emit('server decision response', message);
-      addLog(message, db, 'vote')
-    });
-
-    socket.on('action on object', function(message) {
-      addLog(message, db, 'transaction')
-      socket.broadcast.emit('server action on object', message);
-    });
-  });
-}
-
-module.exports.sockets = sockets;
+*/
